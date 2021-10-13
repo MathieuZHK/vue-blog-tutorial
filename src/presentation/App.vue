09a7aa784be87816f850b32a5061aae77194b845
f.marie-louise <fabien.marie-louise@symaris.fr>
2021-10-13 09:29:58 +0200

    step 01

---
+++ src/presentation/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import NavBar from "@/presentation/components/NavBar.vue";
+</script>
+
+<template>
+  <main>
+    <NavBar />
+    <div class="content">
+      <router-view></router-view>
+    </div>
+  </main>
+</template>
+
+<style>
+@import "../index.css";
+</style>
---
+++ src/presentation/components/NavBar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar">
+    <h1>The Vue Blog</h1>
+    <div class="links">
+      <RouterLink to="/" exact-active-class="is--active">Home</RouterLink>
+      <RouterLink to="/create-post" exact-active-class="is--active">
+        Create Post
+      </RouterLink>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  max-width: 960px;
+  margin: 0 auto;
+  border-bottom: 1px solid #f2f2f2;
+}
+
+.navbar h1 {
+  font-weight: 700;
+  color: #059669;
+}
+
+.navbar .links {
+  margin-left: auto;
+}
+
+.navbar a {
+  margin-left: 16px;
+  text-decoration: none;
+  padding: 6px;
+}
+
+.navbar a:hover {
+  color: #059669;
+}
+
+.navbar a.is--active {
+  color: #059669;
+}
+</style>
